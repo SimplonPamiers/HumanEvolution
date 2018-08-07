4a3bfb15b6403a7ab1b09040169270bf0de6f3c7
Perendevy <perendevy.loraine@gmail.com>
2018-08-07 14:19:52 +0200

    la

--- js/chrono.js
+++ js/chrono.js
@@ -19,4 +19,20 @@ function evolution() {
     $(".taille").html(taille + ' cm');
     age++;
     taille = taille + 2;
+    if (age>=3){
+    taille=taille +20;    
+    }
+
 }
+
+//si l'age est <3  alors  ajoute +20cm 
+//   if ( age <3 )
+//     add 20 cm
+// else if ( 4<age>12)
+//     add 5 cm
+// else if (13<age>17)
+//     add 2 cm
+// else (71<age>100)
+//     sous 0,1 cm//
+
+
