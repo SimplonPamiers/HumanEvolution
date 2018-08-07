96315dfb290869d089365620a3e26206456dcc1f
BlaadFrost <lepezlukas.pro@gmail.com>
2018-08-07 17:24:27 +0200

    le super push de la mort qui tue

--- js/chrono.js
+++ js/chrono.js
@@ -1,5 +1,25 @@
+function generateAvatar() {
+    lifeSpan = Math.floor(Math.random()*100);
+    console.log ('lifespan', lifeSpan);
+    
+    birthSize = (Math.random() * (57 - 42) + 42).toFixed(1);
+    console.log('birthSize',birthSize); 
+    
+    growth = Math.random() * (1.2 - 0.8) + 0.8;
+    console.log('growth',growth);
+
+    var sexArray = ['Homme /','Femme /'];
+    genre = sexArray[Math.floor(Math.random()*2)];
+    console.log(genre);   
+    
+    $(".taille").html(birthSize + ' cm');
+    $(".sexe").html(genre);
+}
+
+$(document).ready(generateAvatar);
+
 var age = parseInt($(".age").html());
-var taille = parseInt($(".taille").html());
+var taille = parseFloat($(".taille").html());
 var sexe = $(".sexe").html().substring(0,5);
 
 
@@ -17,47 +37,34 @@ function chrono() {
 }
 
 function evolution() {
+    
     $(".age").html(age + ' ans / ');
     $(".taille").html(taille.toFixed(1) + ' cm');
+
     age++;
 
-    if (age <= 3) {
-        taille = taille + 20;
-        $(".img").attr('src', 'img/bebe.svg');
-    } else if (age <= 12) {
-        taille = taille + 5;
-        $(".img").attr('src', 'img/enfant.svg');
-    } else if (age <= 17) {
-            taille = taille + 2;
-            $(".img").attr('src', 'img/' + sexe + 'A.svg');
-    } else if (age <= 50) {
-        taille = taille + 0;
-        $(".img").attr('src', 'img/' + sexe + 'J.svg');
-    }   else if (age <= 70) {
-        taille = taille + 0;
-        $(".img").attr('src', 'img/' + sexe + 'M.svg');
+    console.log(taille);
+
+    if (age < lifeSpan ) {
+        if (age <= 3) {
+            taille = taille + (20*growth);
+            $(".img").attr('src', 'img/bebe.svg');
+        } else if (age <= 12) {
+            taille = taille + (5*growth);
+            $(".img").attr('src', 'img/enfant.svg');
+        } else if (age <= 17) {
+                taille = taille + 2;
+                $(".img").attr('src', 'img/' + sexe + 'A.svg');
+        } else if (age <= 50) {
+            $(".img").attr('src', 'img/' + sexe + 'J.svg');
+        }   else if (age <= 70) {
+            $(".img").attr('src', 'img/' + sexe + 'M.svg');
+        } else {
+            taille = taille - 0.1;
+            $(".img").attr('src', 'img/' + sexe + 'V.svg');
+        };
     } else {
-        taille = taille - 0.1;
-        $(".img").attr('src', 'img/' +sexe + 'V.svg');
-    };
+        $(".img").attr('src', 'img/rip.svg');
+        age = lifeSpan;
+    }
 };
-
-
-
-
-function generateAvatar() {
-    var lifeSpan = Math.floor(Math.random()*100);
-    console.log ('lifespan', lifeSpan);
-    
-    var birthSize = (Math.random() * (57 - 42) + 42).toFixed(1);
-    console.log('birthSize',birthSize); 
-    
-    var growth = Math.random() * (1.2 - 0.8) + 0.8;
-    console.log('growth',growth);
-
-    var sexArray = ['Homme','Femme'];
-    var genre = sexArray[Math.floor(Math.random()*2)];
-    console.log(genre);    
-}
-
-generateAvatar();
