7e4b713da1895cbea52595dfd8163d283ea07b76
Perendevy <perendevy.loraine@gmail.com>
2018-08-07 15:08:24 +0200

    modif

--- js/chrono.js
+++ js/chrono.js
@@ -1,5 +1,6 @@
 var age = parseInt($(".age").html());
 var taille = parseInt($(".taille").html());
+var sexe = parseInt($(".sexe").html())
 
 function chrono() {
     var year = parseInt($(".chrono").html());
@@ -18,28 +19,8 @@ function evolution() {
     $(".age").html(age + ' ans / ');
     $(".taille").html(taille.toFixed(1) + ' cm');
     age++;
-<<<<<<< HEAD
-    taille = taille + 2;
-    if (age>=3){
-    taille=taille +20;    
-    }
-
-}
-
-//si l'age est <3  alors  ajoute +20cm 
-//   if ( age <3 )
-//     add 20 cm
-// else if ( 4<age>12)
-//     add 5 cm
-// else if (13<age>17)
-//     add 2 cm
-// else (71<age>100)
-//     sous 0,1 cm//
-
-
-=======
-
-    if (age <= 3) {
+if (sexe===homme){
+     if (age <= 3 === ) {
         taille = taille + 20;
     } else if (age <= 12) {
         taille = taille + 5;
@@ -50,5 +31,8 @@ function evolution() {
     } else {
         taille = taille - 0.1;
     };
+}
+   
+
 };
->>>>>>> 782e6ea8dc4d7e77c8c887136a2a9178ba703273
+
