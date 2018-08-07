5e4b9e270f49e40bede5d9c659eef01a82750526
BlaadFrost <lepezlukas.pro@gmail.com>
2018-08-07 12:42:57 +0200

    salut

--- js/chrono.js
+++ js/chrono.js
@@ -1,11 +1,22 @@
+var age = parseInt($(".age").html());
+var taille = parseInt($(".taille").html());
+
 function chrono(){
     var year = parseInt($(".chrono").html());
     
     var chrono = setInterval(function(){
         $(".chrono").html(year);
+        evolution();
         year++;
         if(year>2000){
             clearInterval(chrono);
         }
     }, 100);
+}
+
+function evolution() {
+    $(".age").html(age + ' ans / ');
+    $(".taille").html(taille + ' cm');
+    age++;
+    taille = taille + 2;
 }
