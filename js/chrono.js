1040f2bfca8d361e921ce8406d365450faf82875
thomas <onixyros@gmail.com>
2018-08-07 15:07:37 +0200

    added homme femme img

--- js/chrono.js
+++ js/chrono.js
@@ -1,5 +1,7 @@
 var age = parseInt($(".age").html());
 var taille = parseInt($(".taille").html());
+var sexe = $(".sexe").html().substring(0,5);
+
 
 function chrono() {
     var year = parseInt($(".chrono").html());
@@ -21,13 +23,21 @@ function evolution() {
 
     if (age <= 3) {
         taille = taille + 20;
+        $(".img").attr('src', 'img/bebe.svg');
     } else if (age <= 12) {
         taille = taille + 5;
+        $(".img").attr('src', 'img/enfant.svg');
     } else if (age <= 17) {
-        taille = taille + 2;
-    } else if (age <= 70) {
+            taille = taille + 2;
+            $(".img").attr('src', 'img/' + sexe + 'A.svg');
+    } else if (age <= 50) {
         taille = taille + 0;
+        $(".img").attr('src', 'img/' + sexe + 'J.svg');
+    }   else if (age <= 70) {
+        taille = taille + 0;
+        $(".img").attr('src', 'img/' + sexe + 'M.svg');
     } else {
         taille = taille - 0.1;
+        $(".img").attr('src', 'img/' +sexe + 'V.svg');
     };
 };
