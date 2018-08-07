85ae5196c97d77de4c3132fb23e39d7be2584bc3
thomas <onixyros@gmail.com>
2018-08-07 12:15:28 +0200

    added chrono.js

---
+++ js/chrono.js
@@ -0,0 +1,19 @@
+var chrono_content = 1900;
+$( ".chrono" ).html(chrono_content);
+
+$( ".btn" ).click(function chrono() 
+    {
+        var inter = setInterval(function()
+        {
+            if (chrono_content < 2000) 
+            { 
+                chrono_content++;
+                $( ".chrono" ).html(chrono_content);
+            }
+            else
+            {
+            clearInterval(inter);
+            }
+        }, 100);
+    });
+        
