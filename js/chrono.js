782e6ea8dc4d7e77c8c887136a2a9178ba703273
PhilippineG <philippinegilabert@gmail.com>
2018-08-07 14:17:48 +0200

    sprint4

--- js/chrono.js
+++ js/chrono.js
@@ -1,14 +1,14 @@
 var age = parseInt($(".age").html());
 var taille = parseInt($(".taille").html());
 
-function chrono(){
+function chrono() {
     var year = parseInt($(".chrono").html());
-    
-    var chrono = setInterval(function(){
+
+    var chrono = setInterval(function() {
         $(".chrono").html(year);
         evolution();
         year++;
-        if(year>2000){
+        if (year > 2000) {
             clearInterval(chrono);
         }
     }, 100);
@@ -16,7 +16,18 @@ function chrono(){
 
 function evolution() {
     $(".age").html(age + ' ans / ');
-    $(".taille").html(taille + ' cm');
+    $(".taille").html(taille.toFixed(1) + ' cm');
     age++;
-    taille = taille + 2;
-}
+
+    if (age <= 3) {
+        taille = taille + 20;
+    } else if (age <= 12) {
+        taille = taille + 5;
+    } else if (age <= 17) {
+        taille = taille + 2;
+    } else if (age <= 70) {
+        taille = taille + 0;
+    } else {
+        taille = taille - 0.1;
+    };
+};
