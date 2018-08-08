259e4af1e29cf68d7380e37a7f229fad9968b9f0
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-08-09 00:02:14 +0200

    removed switch case

--- js/chrono.js
+++ js/chrono.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             avatar.num = i;
             avatar.age = 0;
             avatar.lifeSpan= Math.floor(Math.random()*100);
-            avatar.birthSize = (Math.random() * (57 - 42) + 42).toFixed(1);
+            avatar.height = (Math.random() * (57 - 42) + 42).toFixed(1);
             avatar.growth = Math.random() * (1.2 - 0.8) + 0.8;
             avatar.gender = genderType[Math.floor(Math.random()*2)]
             // console.log('avatar',i,' ',avatar);
@@ -21,7 +21,7 @@ $(document).ready(function () {
     function createCells(obj) {
 
         obj.map((obj,i) => {
-            $('.wrapper').append("<div class='grid_col'><div id='cell#"+i+"' class='row'><img id=img#'"+i+"' class='img' src = 'img/bebe.svg'/></div><div class='row responsive_row'><span class='sexe'>"+obj.gender+" / </span><span class='age'>"+obj.age+" ans /</span><span class='taille'>"+obj.birthSize+"</span></div></div>");
+            $('.wrapper').append("<div class='grid_col'><div id='cell#"+i+"' class='row'><img id=img#'"+i+"' class='img' src = 'img/bebe.svg'/></div><div class='row responsive_row'><span id='sex#"+i+"'class='sexe'>"+obj.gender+" / </span><span class='age'>"+obj.age+" ans /</span><span class='taille'>"+obj.height+"</span></div></div>");
         })
     }
 
@@ -61,44 +61,41 @@ function chrono() {
 }
 
 function evolution(object) {
-
-    console.log('evolution age',object.age);
-    console.log('evolution birthSize',object.birthSize);
+    // console.log('evolution num %s /',object.num);
+    // console.log('evolution age %s /',object.age);
+    // console.log('evolution height %s /',object.height);
     
-    let age = object.age;
-    console.log('age',age);
-    let taille = object.birthSize;
-    let i = object.num
-    age++;
-    console.log('age++',age);
+    object.age++;
+    $('#sex#'+object.num).html(object.age);
 
-    switch (age){
-        case (age == object.lifeSpan) :
-            $('#img#'+i).attr('src', 'img/rip.svg');
-            age = object.lifeSpan;
-            break;
-        case (age <= 3) :
-            taille = taille + (20*object.growth);
-            $('#img#'+i).attr('src', 'img/bebe.svg');
-            break;
-        case (3 > age <= 12) :
-            taille = taille + (5*object.growth);
-            $('#img#'+i).attr('src', 'img/enfant.svg');
-            break;
-        case (12 > age <= 17) :
-            taille = taille + 2;
-            $('#img#'+i).attr('src', 'img/' + object.gender + 'A.svg');
-            break;
-        case (17 > age <= 50) :
-            $('#img#'+i).attr('src', 'img/' + sexe + 'J.svg');
-            break;
-        case (50 > age <= 70) :
-            $('#img#'+i).attr('src', 'img/' + sexe + 'M.svg');
-            break;
-        case (70 > age) :
-            $('#img#'+i).attr('src', 'img/' + sexe + 'V.svg');
-            break;
-        default:
-            console.log('evolution : no object found');
+
+    if (object.age == object.lifeSpan){
+        $('#img#'+object.num).attr('src', 'img/rip.svg');
+        object.age = object.lifeSpan;
+        console.log('avatar %s is dead at age %s / lifespan was %s',object.num, object.age, object.lifeSpan)
     }
+    else if (object.age <= 3) {
+        object.height = object.height + (20*object.growth);
+        $('#img#'+object.num).attr('src', 'img/bebe.svg');
+    }
+    else if (3 > object.age <= 12) {
+        object.height = object.height + (5*object.growth);
+        $('#img#'+object.num).attr('src', 'img/enfant.svg');
+    }
+    else if (12 > object.age <= 17) {
+        object.height = object.height + 2;
+        $('#img#'+object.num).attr('src', 'img/' + object.gender + 'A.svg');
+
+    }
+    else if (17 > object.age <= 50) {
+        $('#img#'+object.num).attr('src', 'img/' + sexe + 'J.svg');
+
+    }
+    else if (50 > object.age <= 70) {
+        $('#img#'+object.num).attr('src', 'img/' + sexe + 'M.svg');       
+    }
+    else {
+        $('#img#'+object.num).attr('src', 'img/' + sexe + 'V.svg');
+    }
+
 };
