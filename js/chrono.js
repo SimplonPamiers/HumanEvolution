43b35bad7a464cfb8360f14c53d65189f87981a1
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-08-08 23:27:42 +0200

    refactorinng

--- js/chrono.js
+++ js/chrono.js
@@ -1,62 +1,37 @@
 $(document).ready(function () {
 
-    var newAvatars = [];
-
-
-    // let avatar1 = creationCellule()[0];
-    // let avatar2 = creationCellule()[2];
-
-    // console.log('avatar 1',avatar1 );
-    // console.log('avatar 2',avatar2 );
-
-    for (i = 1; i < 10; i++) {
-        let avatar;
-        avatar = creationCellule()[i];
-        let newAvatar = []
-        newAvatar.age = avatar.age;
-        console.log('avatar.age :',avatar.age)
-        newAvatar.lifeSpan= avatar.lifeSpan;
-        newAvatar.taille = avatar.taille;
-        newAvatar.growth = avatar.growth;
-        newAvatar.sexe = avatar.sexe;
-        newAvatars.push(newAvatar)
-        console.log('NewAvatar',i,' ',avatar)
-    }
-
-
-    function creationCellule() {
-        let i
+    async function createAvatars() {
         const avatars = [];
-
+        const genderType = ['Homme','Femme'];
+        let i
         for (i = 1; i < 10; i++) {
-
-            let avatar = {}
-
-            avatar.lifeSpan = Math.floor(Math.random()*100);
-            // console.log ('lifespan avatar#%s, %s',i, avatar.lifeSpan);
-
-            avatar.birthSize = (Math.random() * (57 - 42) + 42).toFixed(1);
-            // console.log('birthSize avatar#%s, %s',i,avatar.birthSize); 
-            
-            avatar.growth = Math.random() * (1.2 - 0.8) + 0.8;
-            // console.log('growth avatar#%s, %s',i,avatar.growth);
-        
-            var sexArray = ['Homme','Femme'];
-            avatar.genre = sexArray[Math.floor(Math.random()*2)];
-            // console.log('genre avatar#%s, %s',i,avatar.genre);   
-            
-            $('#cell'+i).find( ".taille" ).html(avatar.birthSize + ' cm');
-            $('#cell'+i).find(".sexe").html(avatar.genre);
+            let avatar = {};
+            avatar.num = i;
             avatar.age = 0;
-
-            avatars.push(avatar);
-
-            $('.wrapper').append("<div class='grid_col'><div id='cell#"+i+"' class='row'><img class='img' src='img/bebe.svg'/></div><div class='row responsive_row'><span class='sexe'>"+avatar.genre+"/ </span><span class='age'>"+avatar.age+" ans /</span><span class='taille'>"+avatar.birthSize+"</span></div></div>");
+            avatar.lifeSpan= Math.floor(Math.random()*100);
+            avatar.birthSize = (Math.random() * (57 - 42) + 42).toFixed(1);
+            avatar.growth = Math.random() * (1.2 - 0.8) + 0.8;
+            avatar.gender = genderType[Math.floor(Math.random()*2)]
+            // console.log('avatar',i,' ',avatar);
+            avatars.push(avatar)
         }
-        console.log('Avatars',avatars);   
-
         return avatars
     }
+
+    function createCells(obj) {
+
+        obj.map((obj,i) => {
+            $('.wrapper').append("<div class='grid_col'><div id='cell#"+i+"' class='row'><img id=img#'"+i+"' class='img' src = 'img/bebe.svg'/></div><div class='row responsive_row'><span class='sexe'>"+obj.gender+" / </span><span class='age'>"+obj.age+" ans /</span><span class='taille'>"+obj.birthSize+"</span></div></div>");
+        })
+    }
+
+    createAvatars().then(res => {
+        console.log('response is true',res)
+        createCells(res)
+        sessionStorage.setItem('avatars', JSON.stringify(res));
+        console.log('sessionStorage',sessionStorage.getItem('avatars'));
+    });
+
 });
 
 // $( ".btn" ).click(function (newAvatars) {
@@ -64,55 +39,66 @@ $(document).ready(function () {
 //     chrono(newAvatars);
 // });
 
-function chrono(newAvatars) {
-    console.log('chrono newAvatars',newAvatars);
+function chrono() {
     console.log('click on chrono');
     var year = parseInt($(".chrono").html());
-    var chrono = setInterval(function() {
-    $(".chrono").html(year);
-        for (i = 1; i < 10; i++) {
-            newAvatars[i];
-            let age = newAvatars.age;
-            let lifeSpan= newAvatars.lifeSpan;
-            let taille = newAvatars.taille;
-            let growth = newAvatars.growth;
-            let sexe = newAvatars.sexe;
-            evolution(i,age,lifeSpan,taille,growth,sexe);
+    const avatarList = JSON.parse(sessionStorage.getItem('avatars'));
+    console.log('avatarsObject',avatarList);
 
-            console.log('avatar.chrono',age,lifeSpan,taille,growth,sexe)
+    var chrono = setInterval(function() {
+        $(".chrono").html(year);
+
+        avatarList.map((avatar) => {
+            evolution(avatar);
+        })
+
+        year++;
+        if (year > 2000) {
+            clearInterval(chrono);
         }
-    year++;
-    if (year > 2000) {
-        clearInterval(chrono);
-    }
     }, 100)
 
 }
 
-function evolution(i,age,lifeSpan,taille,growth,sexe) {
+function evolution(object) {
 
+    console.log('evolution age',object.age);
+    console.log('evolution birthSize',object.birthSize);
+    
+    let age = object.age;
+    console.log('age',age);
+    let taille = object.birthSize;
+    let i = object.num
     age++;
+    console.log('age++',age);
 
-    if (age < lifeSpan ) {
-        if (age <= 3) {
-            taille = taille + (20*growth);
-            $('#cell#'+i).find(".img").attr('src', 'img/bebe.svg');
-        } else if (age <= 12) {
-            taille = taille + (5*growth);
-            $('#cell#'+i).find(".img").attr('src', 'img/enfant.svg');
-        } else if (age <= 17) {
-                taille = taille + 2;
-                $('#cell#'+i).find(".img").attr('src', 'img/' + sexe + 'A.svg');
-        } else if (age <= 50) {
-            $('#cell#'+i).find(".img").attr('src', 'img/' + sexe + 'J.svg');
-        }   else if (age <= 70) {
-            $('#cell#'+i).find(".img").attr('src', 'img/' + sexe + 'M.svg');
-        } else {
-            taille = taille - 0.1;
-            $('#cell#'+i).find(".img").attr('src', 'img/' + sexe + 'V.svg');
-        };
-    } else {
-        $('#cell#'+i).find(".img").attr('src', 'img/rip.svg');
-        age = lifeSpan;
+    switch (age){
+        case (age == object.lifeSpan) :
+            $('#img#'+i).attr('src', 'img/rip.svg');
+            age = object.lifeSpan;
+            break;
+        case (age <= 3) :
+            taille = taille + (20*object.growth);
+            $('#img#'+i).attr('src', 'img/bebe.svg');
+            break;
+        case (3 > age <= 12) :
+            taille = taille + (5*object.growth);
+            $('#img#'+i).attr('src', 'img/enfant.svg');
+            break;
+        case (12 > age <= 17) :
+            taille = taille + 2;
+            $('#img#'+i).attr('src', 'img/' + object.gender + 'A.svg');
+            break;
+        case (17 > age <= 50) :
+            $('#img#'+i).attr('src', 'img/' + sexe + 'J.svg');
+            break;
+        case (50 > age <= 70) :
+            $('#img#'+i).attr('src', 'img/' + sexe + 'M.svg');
+            break;
+        case (70 > age) :
+            $('#img#'+i).attr('src', 'img/' + sexe + 'V.svg');
+            break;
+        default:
+            console.log('evolution : no object found');
     }
 };
