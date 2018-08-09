a8b02fefccb403462563af07f82a98d234b7d600
BlaadFrost <lepezlukas.pro@gmail.com>
2018-08-09 18:03:21 +0200

    Initial commit

--- js/chrono.js
+++ js/chrono.js
@@ -1,79 +1,90 @@
 $(document).ready(function () {
 
-    function creationCellule(n) {
-        // Ajoute une div avec le personnage et toutes ses informations
-        $('.wrapper').append("<div class='grid_col'><div class='row'><img class='img img"+ n +"' src='img/bebe.svg'/></div><div class='row responsive_row'><span class='sexe"+ n +"'></span><span class='age"+ n +"'>0 ans /</span><span class='taille"+ n +"'></span></div></div>");
+    tabPersonnage = [];
 
-        lifeSpan = Math.floor(Math.random()*100);
-    
+    progress = $('.progress').val();
+
+    function creationCellule(n) {
+        $('.row').append("<div class='col-xl-4 col-sm-6 col-12'><div class='card rounded w-100 mb-3' style='width: 18rem;'><img class='card-img-top img img" + n + " m-auto' src='img/bebe.svg'><div class='card-body'><div class='text-center'><span class='sexe" + n + "'></span><span>&nbsp;/&nbsp;</span><span class='age" + n + "'>0</span><span>&nbsp;ans&nbsp;/&nbsp;</span><span class='taille" + n + "'></span><span>&nbsp;cm</span></div></div></div></div>");
+
+        lifeSpan = Math.floor(Math.random() * 100);
         birthSize = (Math.random() * (57 - 42) + 42).toFixed(1);
-    
         growth = Math.random() * (1.2 - 0.8) + 0.8;
 
-        var sexArray = ['Homme /','Femme /'];
-
-        genre = sexArray[Math.floor(Math.random()*2)];
+        const sexArray = ['Homme', 'Femme'];
+        genre = sexArray[Math.floor(Math.random() * 2)];
     }
 
-    // Boucle for pour lancer la fonction 9 fois, n étant une variable permettant de vérifier à combien de case nous en sommes.
     for (n = 0; n < 9; n++) {
-        creationCellule(n); 
-
-        $(".taille" + n).html(birthSize + ' cm');
+        creationCellule(n);
+        $(".taille" + n).html(birthSize);
         $(".sexe" + n).html(genre);
-        var age = parseInt($(".age" + n).html());
-        var taille = parseFloat($(".taille" + n).html());
-        var sexe = $(".sexe" + n).html().substring(0,5);
-
-        all = [age, taille, sexe, growth, lifeSpan];
-        tabPersonnage.push(all);   
+        let taille = parseFloat($(".taille" + n).html());
+        let sexe = $(".sexe" + n).html();
+        let all = [0, taille, sexe, growth, lifeSpan];
+        tabPersonnage.push(all);
     }
-    console.log(tabPersonnage);
 });
 
-tabPersonnage = [];
-
 function evolution(n) {
-    
-    $(".age" + n).html(tabPersonnage[n][0] + ' ans / ');
-    $(".taille" + n).html(tabPersonnage[n][1].toFixed(1) + ' cm');
+    $(".age" + n).html(tabPersonnage[n][0]);
+    $(".taille" + n).html(tabPersonnage[n][1].toFixed(1));
 
     tabPersonnage[n][0]++;
 
-    if (tabPersonnage[n][0] < tabPersonnage[n][4] ) {
-        if (tabPersonnage[n][0] <= 3) {
-            tabPersonnage[n][1] = tabPersonnage[n][1] + (20*growth);
-            $(".img" + n).attr('src', 'img/bebe.svg');
-        } else if (tabPersonnage[n][0] <= 12) {
-            tabPersonnage[n][1] = tabPersonnage[n][1] + (5*growth);
-            $(".img" + n).attr('src', 'img/enfant.svg');
-        } else if (tabPersonnage[n][0] <= 17) {
-            tabPersonnage[n][1] = tabPersonnage[n][1] + 2;
-            $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'A.svg');
-        } else if (tabPersonnage[n][0] <= 50) {
-            $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'J.svg');
-        }   else if (tabPersonnage[n][0] <= 70) {
-            $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'M.svg');
-        } else {
-            tabPersonnage[n][1] = tabPersonnage[n][1] - 0.1;
-            $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'V.svg');
-        };
-    } else {
-        $(".img" + n).attr('src', 'img/rip.svg');
-        tabPersonnage[n][0] = tabPersonnage[n][4];
+    switch (true) {
+
+        case tabPersonnage[n][0] < tabPersonnage[n][4]:
+            switch (true) {
+
+                case tabPersonnage[n][0] <= 3:
+                    tabPersonnage[n][1] = tabPersonnage[n][1] + (20 * growth);
+                    $(".img" + n).attr('src', 'img/bebe.svg');
+                    break;
+
+                case tabPersonnage[n][0] <= 12:
+                    tabPersonnage[n][1] = tabPersonnage[n][1] + (5 * growth);
+                    $(".img" + n).attr('src', 'img/enfant.svg');
+                    break;
+                case tabPersonnage[n][0] <= 17:
+                    tabPersonnage[n][1] = tabPersonnage[n][1] + 2;
+                    $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'A.svg');
+                    break;
+                case tabPersonnage[n][0] <= 50:
+                    $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'J.svg');
+                    break;
+                case tabPersonnage[n][0] <= 70:
+                    $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'M.svg');
+                    break;
+
+                default:
+                    tabPersonnage[n][1] = tabPersonnage[n][1] - 0.1;
+                    $(".img" + n).attr('src', 'img/' + tabPersonnage[n][2] + 'V.svg');
+                    break;
+            }
+            break;
+
+        default:
+            $(".img" + n).attr('src', 'img/rip.svg');
+            tabPersonnage[n][0] = tabPersonnage[n][4];
+            break;
     }
-};
+}
 
 function chrono() {
-    var year = parseInt($(".chrono").html());
-    var chrono = setInterval(function() {
+    let year = parseInt($(".chrono").html());
+    let yearMax = 2000;
+    let chrono = setInterval(function () {
         $(".chrono").html(year);
         for (n = 0; n < 9; n++) {
             evolution(n);
         }
         year++;
-        if (year > 2000) {
+        progress = progress + 1 / 100 * yearMax;
+        $('.progress').val(progress).attr('max', yearMax);
+        if (year > yearMax) {
             clearInterval(chrono);
+            $('.rejouer').removeClass('d-none');
         }
     }, 100);
 }
